b5cd1b3a7b24eed8f2a1e13b0956ad7958716367
IN-LL-029 <IN-LL-029@IN-LL-029>
2019-08-27 15:52:18 +0200

    commit: correction rouge

---
+++ src/main/java/fr/formation/inti/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package fr.formation.inti.service;
+
+import java.util.List;
+
+import fr.formation.inti.dao.EmployeeDaoImpl;
+import fr.formation.inti.entities.Employee;
+
+public class DepartmentService {
+	
+
+}
